78622e627540b502d92b6d40d5a1c9df79d55e3b
Peter Owusu <odenehonanawusu77@gmail.com>
2023-07-18 22:35:33 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -198,6 +198,10 @@ class Dev(Configuration):
         'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
     
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    
+    ACCOUNT_ACTIVATION_DAYS = 7
+
 class Prod(Dev):
   DEBUG = False
   SECRET_KEY = values.SecretValue()
